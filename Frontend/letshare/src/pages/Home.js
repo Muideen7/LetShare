37c7b4a898ab7b11edd701c85c7f5a15e5863e01
Roni <ronniiii@yahoo.com>
2023-09-27 19:16:33 +0100

    [UPDATE]setup home page

--- Frontend/letshare/src/pages/Home.js
+++ Frontend/letshare/src/pages/Home.js
@@ -1,1 +1,16 @@
-rfce
+import Hero from "../components/Hero/Hero"
+import Features from "../components/Features/Features";
+import Growth from "../components/Growth/Growth";
+import Questions from "../components/Questions/Questions";
+
+const Home = () => {
+  return (
+    <div>
+        <Hero />
+        <Features />
+        <Growth />
+        <Questions />
+    </div>
+  );
+};
+export default Home;
